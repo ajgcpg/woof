55aae4fbe2669218628c768efaa8e9552c8594d3
Aidan Gallagher <ajgcpg1@gmail.com>
2017-10-22 02:30:50 -0400

    shit

--- models/user.js
+++ models/user.js
@@ -2,8 +2,8 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
-    email {type: String, required: true},
-    userName {type: String, required: true},
+    email: {type: String, required: true},
+    userName: {type: String, required: true},
     password: {type: String, required: true}
 });
 
